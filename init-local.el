9c0739723e7f08516049e213a20540b2d20631d9
David Corking <dmc-source@dcorking.com>
2013-08-09 07:49:02 +0100

    Fix and remove duplications from local
     customizations init-local.el

--- init-local.el
+++ init-local.el
@@ -1,33 +1,14 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; START: Ruby / Rails setup
- 
-;; Interactively Do Things (highly recommended, but not strictly required)
- 
-(ido-mode t)
- 
-;; Rinari
-(push "~/.emacs.d/site-lisp/rinari" load-path)
-(require 'rinari)
- 
+(provide 'init-local)
+
 ;; rails reloaded plugin
 (push "~/.emacs.d/site-lisp/rails-reloaded" load-path)
 (require 'rails-autoload)
 
-;; END: Ruby / Rails setup
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'package)
-(setq package-archives '(
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
-
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(safe-local-variable-values (quote ((eval checkdoc-minor-mode 1))))
  '(version-control t))
 (custom-set-faces
