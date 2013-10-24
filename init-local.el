59c628ba164179e13ee5b45f82f65f103c1a5256
David Corking <dmc-source@dcorking.com>
2013-10-24 14:15:18 +0100

    I think this is a better way to install the smart-tabs-mode package.

--- init-local.el
+++ init-local.el
@@ -3,5 +3,3 @@
 ;; rails reloaded plugin
 (push "~/.emacs.d/site-lisp/rails-reloaded" load-path)
 (require 'rails-autoload)
-(smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
-                       'ruby 'nxml)
---
+++ init-smart-tabs.el
@@ -0,0 +1,6 @@
+(require-package 'smart-tabs-mode)
+
+(smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
+                      'ruby 'nxml)
+
+(provide 'init-smart-tabs)
