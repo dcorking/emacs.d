958b7b0b14bacf070951f34813c57dc8c85d461a
David Corking <dmc-source@dcorking.com>
2013-10-24 14:06:26 +0100

    Custom set variables belong in custom.el
    not init-local.el

--- init-local.el
+++ init-local.el
@@ -5,17 +5,3 @@
 (require 'rails-autoload)
 (smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
                        'ruby 'nxml)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((eval checkdoc-minor-mode 1))))
- '(version-control t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
