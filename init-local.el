4c2452a3d503a11fd3335491615eec29168ecc88
David Corking <dmc-source@dcorking.com>
2013-10-24 13:43:44 +0100

    Installed smart-tabs-mode for supported languages

--- init-local.el
+++ init-local.el
@@ -3,6 +3,8 @@
 ;; rails reloaded plugin
 (push "~/.emacs.d/site-lisp/rails-reloaded" load-path)
 (require 'rails-autoload)
+(smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
+                       'ruby 'nxml)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
