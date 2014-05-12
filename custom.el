7fefce1c9c61bc148b01b3e598f1e2bc28141025
David Corking <dmc-source@dcorking.com>
2014-05-12 20:34:53 +0100

    accept custom theme

--- custom.el
+++ custom.el
@@ -3,9 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(global-linum-mode t)
  '(indent-tabs-mode nil)
  '(safe-local-variable-values (quote ((eval checkdoc-minor-mode 1))))
+ '(session-use-package t nil (session))
  '(version-control t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
