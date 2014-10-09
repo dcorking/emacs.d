e9ecc5d3bd92c90e0329f78da93dfc66aac19ba9
David Corking <dmc-source@dcorking.com>
2014-10-09 18:03:58 +0100

    Tweak some customizations

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(flycheck-gcc-language-standard "gnu11")
  '(global-linum-mode t)
  '(indent-tabs-mode nil)
  '(safe-local-variable-values (quote ((eval checkdoc-minor-mode 1))))
