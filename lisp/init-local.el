fadf9aaffbcf6f2ebb3f24050cdb0f53100e5f08
David Corking <dmc-source@dcorking.com>
2014-10-09 18:12:45 +0100

    Add support for Standard ML
    including path to a local binary

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,5 +1,12 @@
-(provide 'init-local)
-
 ;; rails reloaded plugin
 (push "~/.emacs.d/site-lisp/rails-reloaded" load-path)
 (require 'rails-autoload)
+
+;; Standard ML https://class.coursera.org/proglang-003/wiki/view?page=smlEmacsInstall
+(setenv "PATH" (concat "/home/david1/workspace/sml/bin:" (getenv "PATH")))
+(setq exec-path (cons "/home/david1/workspace/sml/bin"  exec-path))
+
+(require-package 'sml-mode)
+(require 'sml-mode)
+
+(provide 'init-local)
