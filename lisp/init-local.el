5aabd2120eb0c66ad204cb033cec6aaefe80221e
David Corking <dmc-source@dcorking.com>
2015-12-06 15:15:42 +0000

    Remove sml path from local init
    as Emacs inherits this from my Bash shell

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,10 +2,6 @@
 (push "~/.emacs.d/site-lisp/rails-reloaded" load-path)
 (require 'rails-autoload)
 
-;; Standard ML https://class.coursera.org/proglang-003/wiki/view?page=smlEmacsInstall
-(setenv "PATH" (concat "/home/david1/workspace/sml/bin:" (getenv "PATH")))
-(setq exec-path (cons "/home/david1/workspace/sml/bin"  exec-path))
-
 (require-package 'sml-mode)
 (require 'sml-mode)
 
