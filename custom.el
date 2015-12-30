b990142dec2864574a28ee977cd3bee511637177
David Corking <dmc-source@dcorking.com>
2015-12-30 13:11:38 +0000

    Mark solarized dark theme as safe

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes
    (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(flycheck-gcc-language-standard "gnu11")
  '(global-linum-mode t)
  '(indent-tabs-mode nil)
