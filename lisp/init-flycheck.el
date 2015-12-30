69d0e85754f89bf4d2ca5d4858bc61321abed42c
David Corking <dmc-source@dcorking.com>
2015-12-30 11:30:08 +0000

    Set flycheck idle time to 800 ms

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,7 +2,7 @@
   (add-hook 'after-init-hook 'global-flycheck-mode)
 
   ;; Override default flycheck triggers
-  (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+  (setq flycheck-check-syntax-automatically '(save idle-change new-line mode-enabled)
         flycheck-idle-change-delay 0.8)
 
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
