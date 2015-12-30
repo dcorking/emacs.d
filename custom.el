9e6045b48b56beb0827ed525dc9c540365d662c9
David Corking <dmc-source@dcorking.com>
2015-12-30 13:11:16 +0000

    Add another safe lisp variable value

--- custom.el
+++ custom.el
@@ -4,11 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (sanityinc-solarized-light)))
- '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(flycheck-gcc-language-standard "gnu11")
  '(global-linum-mode t)
  '(indent-tabs-mode nil)
- '(safe-local-variable-values (quote ((eval checkdoc-minor-mode 1))))
+ '(safe-local-variable-values (quote ((no-byte-compile t) (eval checkdoc-minor-mode 1))))
  '(session-use-package t nil (session))
  '(version-control t))
 (custom-set-faces
