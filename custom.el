9288ce8e35930f7f30107bf30caa02f43b3187c8
David Corking <dmc-source@dcorking.com>
2017-01-25 10:16:34 +0000

    Misc custom tidy-ups and 2 ch indent in web-mode

--- custom.el
+++ custom.el
@@ -6,19 +6,22 @@
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
-   (vector "#657b83" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#fdf6e3"))
+   (vector "#657b83" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"))
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(display-time-day-and-date t)
- '(display-time-mode t)
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(fci-rule-color "#eee8d5")
- '(flycheck-gcc-language-standard "gnu11")
  '(global-linum-mode t)
  '(indent-tabs-mode nil)
+ '(ruby-encoding-map
+   (quote
+    ((us-ascii)
+     (shift-jis . cp932)
+     (shift_jis . cp932)
+     (japanese-cp932 . cp932)
+     (utf-8))))
  '(ruby-insert-encoding-magic-comment nil)
- '(safe-local-variable-values (quote ((no-byte-compile t) (eval checkdoc-minor-mode 1))))
  '(session-use-package t nil (session))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
@@ -42,10 +45,11 @@
      (340 . "#dc322f")
      (360 . "#cb4b16"))))
  '(vc-annotate-very-old-color nil)
- '(version-control t))
+ '(version-control t)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#1d1f21" :foreground "#c5c8c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo")))))
+ )
