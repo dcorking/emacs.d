2e46c0390d79a2b6ad3884936dfd600ddd15b60c
David Corking <dmc-source@dcorking.com>
2018-02-24 14:10:24 +0000

    Update selected pkgs and mark favourite themes as safe

--- custom.el
+++ custom.el
@@ -6,10 +6,10 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
  '(custom-safe-themes
    (quote
-    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(package-selected-packages
    (quote
-    (tidy project-local-variables yagist writeroom-mode whole-line-or-region whitespace-cleanup-mode wgrep vc-darcs unfill undo-tree typescript-mode textile-mode symbol-overlay switch-window smex smarty-mode skewer-mode session scratch rainbow-delimiters projectile prettier-js php-mode page-break-lines org-pomodoro org-fstree org-cliplink nlinum multiple-cursors move-dup mmm-mode markdown-mode magit-gh-pulls list-unicode-display json-mode js-comint ivy-historian ibuffer-vc highlight-escape-sequences guide-key gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe flycheck-color-mode-line expand-region exec-path-from-shell erlang dynamic-spaces disable-mouse diminish diff-hl default-text-scale darcsum csv-mode counsel company-quickhelp command-log-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode cmd-to-echo bug-reference-github browse-kill-ring browse-at-remote beacon avy anzu add-node-modules-path)))
+    (magit-gh-pulls rjsx-mode multi-term sml-mode tidy project-local-variables yagist writeroom-mode whole-line-or-region whitespace-cleanup-mode wgrep vc-darcs unfill undo-tree typescript-mode textile-mode symbol-overlay switch-window smex smarty-mode skewer-mode session scratch rainbow-delimiters projectile prettier-js php-mode page-break-lines org-pomodoro org-fstree org-cliplink nlinum multiple-cursors move-dup mmm-mode markdown-mode list-unicode-display json-mode js-comint ivy-historian ibuffer-vc highlight-escape-sequences guide-key gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe flycheck-color-mode-line expand-region exec-path-from-shell erlang dynamic-spaces disable-mouse diminish diff-hl default-text-scale darcsum csv-mode counsel company-quickhelp command-log-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode cmd-to-echo bug-reference-github browse-kill-ring browse-at-remote beacon avy anzu add-node-modules-path)))
  '(safe-local-variable-values
    (quote
     ((flycheck-disabled-checkers quote
