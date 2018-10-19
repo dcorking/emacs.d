d866ec1981efdd069a8c47643841d187800a9b52
David Corking <dmc-source@dcorking.com>
2018-10-19 10:57:53 +0100

    Turn on spell checking

---
+++ lisp/init-preload-local.el
@@ -0,0 +1,11 @@
+;;; package: --- Preload customizations
+;;; Commentary:
+;;; called by purcell's init.el
+
+;;; Code:
+(message "%s" "Using the preload to turn on spell check support")
+
+(defconst *spell-check-support-enabled* t)
+
+(provide 'init-preload-local)
+;;; init-preload-local ends here
