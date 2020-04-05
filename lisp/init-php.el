1f3dd689295870e2bac8d89e9cd9c99a3edf54d7
David Corking <david.corking@simpleweb.co.uk>
2020-04-05 13:53:00 +0100

    Disable php auto-completion as crashed while typing on macOS
    with Laravel
    
    ```
    Debugger entered--Lisp error: (wrong-type-argument listp ac-php-phptags-index-process-filter)
      ac-php-g--function-map(ac-php-phptags-index-process-filter)
    ```
    
    but it might work fine if wait until ac-php has finished indexing the
    project, i.e. these messages in Emacs:
    
    ```
    ac-php: The project has been successfully re-indexed
    Back to top level
    ac-php: Reloading the autocompletion data from the tags file...
    ac-php: Reloading has been successfully finished
    ```
    
    Perhaps patch ac-php to make it wait. :shrugging:

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -5,9 +5,10 @@
 (when (maybe-require-package 'php-mode)
   (maybe-require-package 'smarty-mode)
 
-  (when (maybe-require-package 'company-php)
-    (after-load 'company
-      (push 'company-ac-php-backend company-backends))))
+  ;; (when (maybe-require-package 'company-php)
+  ;;   (after-load 'company
+  ;;     (push 'company-ac-php-backend company-backends)))
+  )
 
 (provide 'init-php)
 ;;; init-php.el ends here
