7cc3d76dfac9aa002a87b7275e17bd0333b92253
David Corking <dmc-source@dcorking.com>
2020-11-01 17:42:43 +0000

    Open private Firefox window with browse-url commands

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-firefox))
+ '(browse-url-firefox-arguments (quote ("--private-window")))
  '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
  '(custom-safe-themes
    (quote
