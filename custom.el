5bd5346300c5bff81fa57ea6ac3d092bca9b6c46
David Corking <dmc-source@dcorking.com>
2021-01-21 12:14:17 +0000

    Standardize CSS indent to 2

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function 'browse-url-firefox)
  '(browse-url-firefox-arguments '("--private-window"))
+ '(css-indent-offset 2)
  '(custom-enabled-themes '(sanityinc-tomorrow-bright))
  '(custom-safe-themes
    '("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))
