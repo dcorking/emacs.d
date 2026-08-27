b33081d0207bc7c0dcfab739bd35419cdff60764
David Corking <2813357+dcorking@users.noreply.github.com>
2026-08-27 16:39:39 +0100

    Tweak color of default face

--- custom.el
+++ custom.el
@@ -209,6 +209,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 89)) (:foreground "#eaeaea" :background "#000000"))))
  '(tab-bar ((t (:background "#2a2a2a" :foreground "#969896" :height 1.0)))))
 (provide 'custom)
 ;;; custom.el ends here
